697c1de8580153bca285660d2b637358646034a7
Caio Araújo <78751303+Kainato@users.noreply.github.com>
2023-11-13 16:58:44 -0300

    Wrong Import

--- aeons/lib/classes/app/AppTheme.dart
+++ aeons/lib/classes/app/AppTheme.dart
@@ -1,4 +1,4 @@
-import 'package:aeons/classes/colorsData.dart';
+import 'package:aeons/classes/app/ColorsData.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
